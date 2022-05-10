db83cad1076244fbe7fef412a523f0922e8f30e8
Ross A. Baker <ross@rossabaker.com>
2022-05-09 23:23:43 -0400

    Add noop

--- core/src/main/scala/com/rossabaker/otel4s/otel4s.scala
+++ core/src/main/scala/com/rossabaker/otel4s/otel4s.scala
@@ -1,5 +1,7 @@
 package com.rossabaker.otel4s
 
+import cats.Applicative
+
 trait Otel4s[F[_]] {
   def meterProvider: MeterProvider[F]
 }
@@ -44,10 +46,27 @@ trait MeterProvider[F[ _]] {
   def meter(name: String): MeterBuilder[F]
 }
 
+object MeterProvider {
+  def noop[F[_]: Applicative]: MeterProvider[F] =
+    new MeterProvider[F] {
+      def meter(name: String): MeterBuilder[F] =
+        MeterBuilder.noop
+    }
+}
+
 trait MeterBuilder[F[_]] {
   def withVersion(version: String): MeterBuilder[F]
   def withSchemaUrl(schemaUrl: String): MeterBuilder[F]
   def get: F[Meter[F]]
+}
+
+object MeterBuilder {
+  def noop[F[_]](implicit F: Applicative[F]): MeterBuilder[F] =
+    new MeterBuilder[F] {
+      def withVersion(version: String) = this
+      def withSchemaUrl(schemaUrl: String) = this
+      def get = F.pure(Meter.noop)
+    }
 }
 
 trait Meter[F[_]] {
@@ -60,6 +79,25 @@ trait Meter[F[_]] {
 
   // def upDownCounter(name: String): SyncInstrumentBuilder[F, UpDownCounter[F, Long]]
   // def observableUpDownCounter(name: String): ObservableInstrumentBuilder[F, UpDownCounter[F, Long]]
+}
+
+object Meter {
+  def noop[F[_]](implicit F: Applicative[F]): Meter[F] =
+    new Meter[F] {
+      def counter(name: String) = new SyncInstrumentBuilder[F, Counter[F, Long]] {
+        type Self = this.type
+        def withUnit(unit: String) = this
+        def withDescription(description: String) = this
+        def create = F.pure(Counter.noop)
+      }
+
+      def histogram(name: String) = new SyncInstrumentBuilder[F, Histogram[F, Double]] {
+        type Self = this.type
+        def withUnit(unit: String) = this
+        def withDescription(description: String) = this
+        def create = F.pure(Histogram.noop)
+      }
+    }
 }
 
 trait SyncInstrumentBuilder[F[_], A] {
@@ -81,11 +119,23 @@ trait ObservableInstrumentBuilder[F[_], A] {
 trait Counter[F[_], A] {
   def add(value: A, attribute: Attribute[_]*): F[Unit]
 }
+object Counter {
+  def noop[F[_], A](implicit F: Applicative[F]): Counter[F, A] =
+    new Counter[F, A] {
+      def add(value: A, attribute: Attribute[_]*) = F.unit
+    }
+}
 
 trait ObservableCounter[F[_], A]
 
 trait Histogram[F[_], A] {
   def record(value: A, attributes: Attribute[_]*): F[Unit]
+}
+object Histogram {
+  def noop[F[_], A](implicit F: Applicative[F]): Histogram[F, A] =
+    new Histogram[F, A] {
+      def record(value: A, attribute: Attribute[_]*) = F.unit
+    }
 }
 
 trait ObservableGauge[F[_], A]

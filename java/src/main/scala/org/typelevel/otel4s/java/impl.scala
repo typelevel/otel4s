45414d34e5074b7055df6a61644929410bcf944a
Maksim Ochenashko <maxochenashko@gmail.com>
2022-07-02 20:20:38 +0300

    Configure `scalafix` (#20)

--- java/src/main/scala/org/typelevel/otel4s/java/impl.scala
+++ java/src/main/scala/org/typelevel/otel4s/java/impl.scala
@@ -18,15 +18,14 @@ package org.typelevel.otel4s
 package java
 
 import cats.effect.Sync
-
 import io.opentelemetry.api.{OpenTelemetry => JOpenTelemetry}
-import io.opentelemetry.api.common.{Attributes => JAttributes}
 import io.opentelemetry.api.common.{AttributeKey => JAttributeKey}
 import io.opentelemetry.api.common.{AttributeType => JAttributeType}
-import io.opentelemetry.api.metrics.{Meter => JMeter}
-import io.opentelemetry.api.metrics.{LongCounter => JLongCounter}
+import io.opentelemetry.api.common.{Attributes => JAttributes}
 import io.opentelemetry.api.metrics.{DoubleHistogram => JDoubleHistogram}
+import io.opentelemetry.api.metrics.{LongCounter => JLongCounter}
 import io.opentelemetry.api.metrics.{LongUpDownCounter => JLongUpDownCounter}
+import io.opentelemetry.api.metrics.{Meter => JMeter}
 import org.typelevel.otel4s.metrics._
 
 import scala.jdk.CollectionConverters._
@@ -49,8 +48,10 @@ object OtelJava {
       schemaUrl: Option[String] = None
   )(implicit F: Sync[F])
       extends MeterBuilder[F] {
-    def withVersion(version: String) = copy(version = Option(version))
-    def withSchemaUrl(schemaUrl: String) = copy(schemaUrl = Option(schemaUrl))
+    def withVersion(version: String): MeterBuilder[F] =
+      copy(version = Option(version))
+    def withSchemaUrl(schemaUrl: String): MeterBuilder[F] =
+      copy(schemaUrl = Option(schemaUrl))
 
     def get: F[Meter[F]] = F.delay {
       val b = jOtel.meterBuilder(name)
@@ -84,8 +85,8 @@ object OtelJava {
       extends SyncInstrumentBuilder[F, Counter[F, Long]] {
     type Self = CounterBuilderImpl[F]
 
-    def withUnit(unit: String) = copy(unit = Option(unit))
-    def withDescription(description: String) =
+    def withUnit(unit: String): Self = copy(unit = Option(unit))
+    def withDescription(description: String): Self =
       copy(description = Option(description))
 
     def create: F[Counter[F, Long]] = F.delay {
@@ -112,8 +113,8 @@ object OtelJava {
       extends SyncInstrumentBuilder[F, Histogram[F, Double]] {
     type Self = HistogramBuilderImpl[F]
 
-    def withUnit(unit: String) = copy(unit = Option(unit))
-    def withDescription(description: String) =
+    def withUnit(unit: String): Self = copy(unit = Option(unit))
+    def withDescription(description: String): Self =
       copy(description = Option(description))
 
     def create: F[Histogram[F, Double]] = F.delay {
@@ -127,7 +128,7 @@ object OtelJava {
   private class HistogramImpl[F[_]](histogram: JDoubleHistogram)(implicit
       F: Sync[F]
   ) extends Histogram[F, Double] {
-    def record(d: Double, attributes: Attribute[_]*) =
+    def record(d: Double, attributes: Attribute[_]*): F[Unit] =
       F.delay(histogram.record(d, toJAttributes(attributes)))
   }
 

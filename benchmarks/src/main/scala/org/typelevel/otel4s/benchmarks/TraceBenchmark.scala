f213be10ff6d1b3c6626fd9852c716dae47decfd
Ross A. Baker <ross@rossabaker.com>
2023-02-07 08:23:19 -0500

    Post-merge fallout and lints and such

--- benchmarks/src/main/scala/org/typelevel/otel4s/benchmarks/TraceBenchmark.scala
+++ benchmarks/src/main/scala/org/typelevel/otel4s/benchmarks/TraceBenchmark.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.typelevel.otel4s.benchmarks
 
 import cats.effect.IO
@@ -32,8 +48,8 @@ class TraceBenchmark {
   @Benchmark
   def inMemoryDisabled(ctx: InMemoryTracer): Unit = {
     import ctx._
-    tracer.noopScope
-      .surround(
+    tracer
+      .noopScope(
         tracer.span("span").use_
       )
       .unsafeRunSync()
@@ -42,8 +58,8 @@ class TraceBenchmark {
   @Benchmark
   def inMemoryEnabled(ctx: InMemoryTracer): Unit = {
     import ctx._
-    tracer.rootScope
-      .surround(
+    tracer
+      .rootScope(
         tracer.span("span").use_
       )
       .unsafeRunSync()

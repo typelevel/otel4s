b793ae43bdc40b5583e74f38f9482d10de483b5f
Ross A. Baker <ross@rossabaker.com>
2023-02-11 00:09:12 -0500

    Refinements to ContextPropagators

--- core/common/src/main/scala/org/typelevel/otel4s/TextMapGetter.scala
+++ core/common/src/main/scala/org/typelevel/otel4s/TextMapGetter.scala
@@ -16,22 +16,7 @@
 
 package org.typelevel.otel4s
 
-import cats.Applicative
-
-trait ContextPropagators[F[_]] {
-  type Context
-
-  def textMapPropagator: TextMapPropagator[F]
-}
-
-object ContextPropagators {
-  type Aux[F[_], C] = ContextPropagators[F] { type Context = C }
-
-  def noop[F[_]: Applicative, C]: ContextPropagators.Aux[F, C] =
-    new ContextPropagators[F] {
-      type Context = C
-
-      def textMapPropagator: TextMapPropagator.Aux[F, C] =
-        TextMapPropagator.noop
-    }
+trait TextMapGetter[A] {
+  def get(carrier: A, key: String): Option[String]
+  def keys(carrier: A): List[String]
 }
--- core/common/src/main/scala/org/typelevel/otel4s/TextMapSetter.scala
+++ core/common/src/main/scala/org/typelevel/otel4s/TextMapSetter.scala
@@ -16,22 +16,6 @@
 
 package org.typelevel.otel4s
 
-import cats.Applicative
-
-trait ContextPropagators[F[_]] {
-  type Context
-
-  def textMapPropagator: TextMapPropagator[F]
-}
-
-object ContextPropagators {
-  type Aux[F[_], C] = ContextPropagators[F] { type Context = C }
-
-  def noop[F[_]: Applicative, C]: ContextPropagators.Aux[F, C] =
-    new ContextPropagators[F] {
-      type Context = C
-
-      def textMapPropagator: TextMapPropagator.Aux[F, C] =
-        TextMapPropagator.noop
-    }
+trait TextMapSetter[A] {
+  def unsafeSet(carrier: A, key: String, value: String): Unit
 }

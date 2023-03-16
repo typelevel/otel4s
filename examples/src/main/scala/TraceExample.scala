52151101e814c5659e1baf9658803735c4cbeb39
Zach McCoy <280974+zmccoy@users.noreply.github.com>
2023-03-16 13:02:48 -0500

    Fill out the trace example

--- examples/src/main/scala/TraceExample.scala
+++ examples/src/main/scala/TraceExample.scala
@@ -19,7 +19,6 @@ import cats.Monad
 import cats.effect.IO
 import cats.effect.IOApp
 import cats.effect.Resource
-import cats.effect.std.Console
 import cats.syntax.all._
 import io.opentelemetry.api.GlobalOpenTelemetry
 import org.typelevel.otel4s.Otel4s
@@ -31,10 +30,6 @@ import org.typelevel.vault.Vault
 
 import scala.concurrent.duration._
 
-trait Work[F[_]] {
-  def request(headers: Map[String, String]): F[Unit]
-}
-
 case class User(name: String, userId: String)
 trait UserDatabase[F[_]] { 
     def readUser(userId: String): F[User]
@@ -44,36 +39,35 @@ trait InstitutionService[F[_]] {
     def getUserIds(institutionId: String): F[List[String]]
 }
 
-object Work {
-  def apply[F[_]: Monad: Tracer: TextMapPropagator: Console]: Work[F] =
-    new Work[F] {
-      def request(headers: Map[String, String]): F[Unit] = {
-        val vault =
-          implicitly[TextMapPropagator[F]].extract(Vault.empty, headers)
-        Tracer[F].currentSpanContext.flatMap { current =>
-          Tracer[F].childOrContinue(SpanContext.fromContext(vault)) {
-            val builder = Tracer[F].spanBuilder("Work.DoWork")
-            current.fold(builder)(builder.addLink(_)).build.use { span =>
-              Tracer[F].currentSpanContext
-                .flatMap(ctx => Console[F].println("Context is " + ctx)) *>
-                span.addEvent("Starting the work.") *>
-                doWorkInternal *>
-                span.addEvent("Finished working.")
-            }
-          }
-        }
-      }
+trait UsersAlg[F[_]] {
+  def getAllUsersForInstitution(institutionId: String): F[List[User]]
+}
 
-      def doWorkInternal =
-        Tracer[F]
-          .span("Work.InternalWork")
-          .surround(
-            Console[F].println("Doin' work")
-          )
+object UserDatabase { 
+    def apply[F[_]: Monad : Tracer]: UserDatabase[F] = new UserDatabase[F] { 
+        def readUser(userId: String): F[User] = Tracer[F].span("Read User from DB").surround { //Can add attribute here and should
+            Monad[F].pure(User("Zach", "12345"))
+        }
+    }
+  }
+
+object InstitutionService { 
+    def apply[F[_]: Monad : Tracer: TextMapPropagator]: InstitutionService[F] = new InstitutionService[F] { 
+        def getUserIds(institutionId: String): F[List[String]] = Tracer[F].span("Read User from DB").surround { 
+          Monad[F].pure(List("12345", "123456"))
+        }
     }
 }
 
-object TracingExample extends IOApp.Simple {
+object UserIdsAlg {
+  def apply[F[_]: Monad : Tracer: TextMapPropagator](institutionService: InstitutionService[F], userDB: UserDatabase[F]): UsersAlg[F] = new UsersAlg[F] { 
+    def getAllUsersForInstitution(institutionId: String): F[List[User]] = Tracer[F].span("Get users for institution").surround { 
+      institutionService.getUserIds(institutionId).flatMap(userIds => userIds.traverse(userDB.readUser))
+    }
+  }
+}
+
+object TraceExample extends IOApp.Simple {
   def globalOtel4s: Resource[IO, Otel4s[IO]] =
     Resource
       .eval(IO(GlobalOpenTelemetry.get))
@@ -83,23 +77,19 @@ object TracingExample extends IOApp.Simple {
     globalOtel4s.use { (otel4s: Otel4s[IO]) =>
       implicit val textMapProp: TextMapPropagator[IO] =
         otel4s.propagators.textMapPropagator
-      otel4s.tracerProvider.tracer("example").get.flatMap {
-        implicit tracer: Tracer[IO] =>
+      otel4s.tracerProvider.tracer("TraceExample").get.flatMap { implicit tracer: Tracer[IO] =>
+          val instService = InstitutionService.apply[IO]
+          val userDB = UserDatabase.apply[IO]
+          val userIdAlg = UserIdsAlg.apply[IO](instService, userDB)
           val resource: Resource[IO, Unit] =
             Resource.make(IO.sleep(50.millis))(_ => IO.sleep(100.millis))
           tracer
-            .resourceSpan("resource")(resource)
+            .resourceSpan("Start up")(resource)
             .surround(
-              Work[IO].request(
-                Map(
-                  "X-B3-TraceId" -> "80f198ee56343ba864fe8b2a57d3eff7",
-                  "X-B3-ParentSpanId" -> "05e3ac9a4f6e3b90",
-                  "X-B3-SpanId" -> "e457b5a2e4d86bd1",
-                  "X-B3-Sampled" -> "1"
-                )
-              )
+              userIdAlg.getAllUsersForInstitution("instId").flatMap(IO.println)
             )
       }
     }
   }
 }
+

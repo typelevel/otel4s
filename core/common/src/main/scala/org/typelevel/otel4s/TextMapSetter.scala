d79337d47fa0cbcb9cf3cc42c7fb9e464f808cb3
April Hyacinth <april.hyacinth@banno.com>
2023-05-12 13:32:28 -0400

    Add documentation about `TextMap{Get,Set,Upda}ter` implicits
    Add documentation about `TextMap{Get,Set,Upda}ter` implicits for
    sequential collection types when duplicate keys are present.

--- core/common/src/main/scala/org/typelevel/otel4s/TextMapSetter.scala
+++ core/common/src/main/scala/org/typelevel/otel4s/TextMapSetter.scala
@@ -23,6 +23,14 @@ import scala.collection.mutable.Buffer
   *
   * A trait that defines a method to set a value in a key-value store.
   *
+  * Implicit instances of `TextMapSetter` are provided for
+  * [[scala.collection.mutable.Map]] and [[scala.collection.mutable.Buffer]]
+  * types. The behavior of `TextMapSetter[Buffer[(String, String)]]` when
+  * duplicate keys are present is unspecified, and may change at any time. In
+  * particular, if the behavior of `Buffer` types with duplicate keys is ever
+  * specified by open telemetry, the behavior of such implicit instances will
+  * be made to match the specification.
+  *
   * @see
   *   See [[TextMapGetter]] to get a value from the carrier
   * @see

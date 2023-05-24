32d9f9268dac1b922a5164377a51cfd619a6d5ae
Lawrence Carvalho <lacarvalho91@gmail.com>
2023-05-24 17:26:54 +0100

    tidy up law tests setup

--- java/common/src/test/scala/org/typelevel/otel4s/java/InstancesSuite.scala
+++ java/common/src/test/scala/org/typelevel/otel4s/java/InstancesSuite.scala
@@ -3,13 +3,16 @@ package org.typelevel.otel4s.java
 import cats.effect.testkit.TestInstances
 import cats.effect.{IO, IOLocal}
 import cats.mtl.laws.discipline.LocalTests
-import munit.{CatsEffectSuite, DisciplineSuite}
+import munit.DisciplineSuite
 import org.scalacheck.Arbitrary.arbString
 import org.typelevel.otel4s.java.instances._
 
-class InstancesSuite extends CatsEffectSuite with DisciplineSuite with TestInstances {
+class InstancesSuite extends DisciplineSuite with TestInstances {
   implicit val ticker: Ticker = Ticker()
-  implicit val ioLocal: IOLocal[String] = IOLocal("").unsafeRunSync()(munitIORuntime)
 
-  checkAll("IOLocal.LocalLaws", LocalTests[IO, String].local[String, Int])
+  unsafeRun {
+    IOLocal("").map {implicit ioLocal =>
+      checkAll("IOLocal.LocalLaws", LocalTests[IO, String].local[String, Int])
+    }
+  }
 }

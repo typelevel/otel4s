849c1e93cb32ccb6cd9593d3416c38e4fc02325b
chr12c <cchhristopher@gmail.com>
2023-06-20 12:05:55 +0100

    more wip

--- testkit/traces/jvm/src/main/scala/org/typelevel/otel4s/testkit/traces/TracesSdk.scala
+++ testkit/traces/jvm/src/main/scala/org/typelevel/otel4s/testkit/traces/TracesSdk.scala
@@ -10,11 +10,27 @@ import io.opentelemetry.sdk.testing.exporter.InMemorySpanExporter
 import io.opentelemetry.sdk.trace.SdkTracerProvider
 import cats.effect.IOLocal
 import org.typelevel.vault.Vault
+import org.typelevel.otel4s.trace.TracerProvider
+import io.opentelemetry.sdk.trace.data.SpanData
+
+import scala.jdk.CollectionConverters._
 
 object TracesSdk {
+  object TracerSuite {
+
+    class Sdk[F[_]: Sync](
+      val provider: TracerProvider[F],
+      exporter: InMemorySpanExporter
+    ) {
+
+      def finishedSpans: F[List[SpanData]] =
+        Sync[F].delay(exporter.getFinishedSpanItems.asScala.toList)
+    }
+  }
+
   def create[F[_]: Sync](
       customize: SdkTracerProviderBuilder => SdkTracerProviderBuilder = identity
-  ): F[TracerSuite.Sdk] = {
+  ): F[TracerSuite.Sdk[F]] = {
     val exporter = InMemorySpanExporter.create()
 
     val builder = SdkTracerProvider
@@ -32,7 +48,7 @@ object TracesSdk {
       )
 
       val provider = TracerProviderImpl.local[F](tracerProvider, propagators)
-      new TracerSuite.Sdk(provider, exporter)
+      new TracerSuite.Sdk[F](provider, exporter)
     }
   }
 }

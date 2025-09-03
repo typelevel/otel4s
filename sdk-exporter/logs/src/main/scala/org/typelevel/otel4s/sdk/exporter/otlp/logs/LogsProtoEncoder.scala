05be50968e182bdb325101404644df8a1721af6b
Maksym Ochenashko <maxochenashko@gmail.com>
2025-09-03 16:13:19 +0300

    sdk-exporter-logs: add `LogsProtoEncoder`

---
+++ sdk-exporter/logs/src/main/scala/org/typelevel/otel4s/sdk/exporter/otlp/logs/LogsProtoEncoder.scala
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2025 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.typelevel.otel4s
+package sdk
+package exporter.otlp
+package logs
+
+import com.google.protobuf.ByteString
+import io.circe.Json
+import org.typelevel.otel4s.sdk.exporter.proto.common.{AnyValue => AnyValueProto}
+import org.typelevel.otel4s.sdk.exporter.proto.common.ArrayValue
+import org.typelevel.otel4s.sdk.exporter.proto.common.KeyValue
+import org.typelevel.otel4s.sdk.exporter.proto.common.KeyValueList
+import org.typelevel.otel4s.sdk.exporter.proto.logs.{LogRecord => LogProto}
+import org.typelevel.otel4s.sdk.exporter.proto.logs.ResourceLogs
+import org.typelevel.otel4s.sdk.exporter.proto.logs.ScopeLogs
+import org.typelevel.otel4s.sdk.exporter.proto.logs.SeverityNumber
+import org.typelevel.otel4s.sdk.exporter.proto.logs_service.ExportLogsServiceRequest
+import org.typelevel.otel4s.sdk.logs.data.LogRecordData
+import scalapb.descriptors.FieldDescriptor
+import scalapb.descriptors.PByteString
+import scalapb.descriptors.PValue
+import scalapb_circe.Printer
+import scodec.bits.ByteVector
+
+/** @see
+  *   [[https://github.com/open-telemetry/opentelemetry-proto/blob/v1.5.0/opentelemetry/proto/logs/v1/logs.proto]]
+  */
+private object LogsProtoEncoder {
+
+  implicit val jsonPrinter: Printer = new ProtoEncoder.JsonPrinter {
+    private val EncodeAsHex = Set("trace_id", "span_id")
+
+    /** The `traceId` and `spanId` byte arrays are represented as case-insensitive hex-encoded strings; they are not
+      * base64-encoded as is defined in the standard Protobuf JSON Mapping. Hex encoding is used for traceId and spanId
+      * fields in all OTLP Protobuf messages, e.g., the Span, Link, LogRecord, etc. messages.
+      *
+      * @see
+      *   [[https://github.com/open-telemetry/opentelemetry-proto/blob/v1.5.0/docs/specification.md#json-protobuf-encoding]]
+      */
+    override def serializeSingleValue(
+        fd: FieldDescriptor,
+        value: PValue,
+        formattingLongAsNumber: Boolean
+    ): Json = {
+      value match {
+        case PByteString(bs) if EncodeAsHex.contains(fd.name) =>
+          Json.fromString(ByteVector(bs.toByteArray()).toHex)
+        case _ =>
+          super.serializeSingleValue(fd, value, formattingLongAsNumber)
+      }
+    }
+  }
+
+  implicit val logRecordDataEncoder: ProtoEncoder[LogRecordData, LogProto] = { log =>
+    val traceId =
+      log.traceContext
+        .map(v => ByteString.copyFrom(v.traceId.toArray))
+        .getOrElse(ByteString.EMPTY)
+
+    val spanId =
+      log.traceContext
+        .map(v => ByteString.copyFrom(v.spanId.toArray))
+        .getOrElse(ByteString.EMPTY)
+
+    def toAnyValueProto(value: AnyValue): AnyValueProto =
+      value match {
+        case string: AnyValue.StringValue =>
+          AnyValueProto(AnyValueProto.Value.StringValue(string.value))
+
+        case boolean: AnyValue.BooleanValue =>
+          AnyValueProto(AnyValueProto.Value.BoolValue(boolean.value))
+
+        case long: AnyValue.LongValue =>
+          AnyValueProto(AnyValueProto.Value.IntValue(long.value))
+
+        case double: AnyValue.DoubleValue =>
+          AnyValueProto(AnyValueProto.Value.DoubleValue(double.value))
+
+        case AnyValue.ByteArrayValueImpl(byteArray) =>
+          AnyValueProto(AnyValueProto.Value.BytesValue(ByteString.copyFrom(byteArray)))
+
+        case list: AnyValue.ListValue =>
+          AnyValueProto(AnyValueProto.Value.ArrayValue(ArrayValue(list.value.map(toAnyValueProto))))
+
+        case map: AnyValue.MapValue =>
+          AnyValueProto(AnyValueProto.Value.KvlistValue(KeyValueList(map.value.map { case (k, v) =>
+            KeyValue(k, Some(toAnyValueProto(v)))
+          }.toSeq)))
+
+        case AnyValue.EmptyValueImpl =>
+          AnyValueProto(AnyValueProto.Value.Empty)
+      }
+
+    LogProto(
+      timeUnixNano = log.timestamp.map(_.toNanos).getOrElse(0L),
+      observedTimeUnixNano = log.observedTimestamp.toNanos,
+      severityNumber = SeverityNumber.fromValue(log.severity.map(_.value).getOrElse(0)),
+      severityText = log.severityText.getOrElse(""),
+      body = log.body.map(toAnyValueProto),
+      attributes = ProtoEncoder.encode(log.attributes.elements),
+      droppedAttributesCount = log.attributes.dropped,
+      flags = 0,
+      traceId = traceId,
+      spanId = spanId,
+    )
+  }
+
+  implicit val logRecordDataToRequest: ProtoEncoder[List[LogRecordData], ExportLogsServiceRequest] = { logs =>
+    val resourceLogs =
+      logs
+        .groupBy(_.resource)
+        .map { case (resource, resourceSpans) =>
+          val scopeLogs: List[ScopeLogs] =
+            resourceSpans
+              .groupBy(_.instrumentationScope)
+              .map { case (scope, logs) =>
+                ScopeLogs(
+                  scope = Some(ProtoEncoder.encode(scope)),
+                  logRecords = logs.map(log => ProtoEncoder.encode(log)),
+                  schemaUrl = scope.schemaUrl.getOrElse("")
+                )
+              }
+              .toList
+
+          ResourceLogs(
+            Some(ProtoEncoder.encode(resource)),
+            scopeLogs,
+            resource.schemaUrl.getOrElse("")
+          )
+        }
+        .toList
+
+    ExportLogsServiceRequest(resourceLogs)
+  }
+
+}
---
+++ sdk-exporter/logs/src/test/scala/org/typelevel/otel4s/sdk/exporter/otlp/logs/LogsJsonCodecs.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2025 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.typelevel.otel4s.sdk.exporter.otlp.logs
+
+import io.circe.Encoder
+import io.circe.Json
+import io.circe.syntax._
+import org.typelevel.otel4s.AnyValue
+import org.typelevel.otel4s.sdk.exporter.otlp.JsonCodecs
+import org.typelevel.otel4s.sdk.logs.data.LogRecordData
+import scodec.bits.ByteVector
+
+// the instances mimic Protobuf encoding
+private object LogsJsonCodecs extends JsonCodecs {
+
+  implicit val logRecordDataJsonEncoder: Encoder[LogRecordData] =
+    Encoder.instance { log =>
+      Json
+        .obj(
+          "timeUnixNano" := log.timestamp.map(_.toNanos).filter(_ > 0).map(_.toString),
+          "observedTimeUnixNano" := log.observedTimestamp.toNanos.toString,
+          "severityNumber" := log.severity.map(_.value).filter(_ > 0),
+          "severityText" := log.severityText.filter(_.nonEmpty),
+          "body" := log.body.map(encodeValue),
+          "attributes" := log.attributes.elements,
+          "droppedAttributesCount" := log.attributes.dropped,
+          "traceId" := log.traceContext.map(_.traceId.toHex),
+          "spanId" := log.traceContext.map(_.spanId.toHex)
+        )
+        .dropNullValues
+    }
+
+  implicit val logRecordDataListJsonEncoder: Encoder[List[LogRecordData]] =
+    Encoder.instance { logs =>
+      val resourceLogs =
+        logs.groupBy(_.resource).map { case (resource, resourceLogs) =>
+          val scopeLogs: Iterable[Json] =
+            resourceLogs
+              .groupBy(_.instrumentationScope)
+              .map { case (scope, logs) =>
+                Json
+                  .obj(
+                    "scope" := scope,
+                    "logRecords" := logs.map(Encoder[LogRecordData].apply),
+                    "schemaUrl" := scope.schemaUrl
+                  )
+                  .dropNullValues
+              }
+
+          Json
+            .obj(
+              "resource" := resource,
+              "scopeLogs" := scopeLogs,
+              "schemaUrl" := resource.schemaUrl
+            )
+            .dropNullValues
+        }
+
+      Json.obj("resourceLogs" := resourceLogs).dropEmptyValues
+    }
+
+  private def encodeValue(value: AnyValue): Json = {
+    value match {
+      case _: AnyValue.EmptyValue             => Json.obj()
+      case string: AnyValue.StringValue       => Json.obj("stringValue" := string.value)
+      case boolean: AnyValue.BooleanValue     => Json.obj("boolValue" := boolean.value)
+      case long: AnyValue.LongValue           => Json.obj("intValue" := long.value.toString)
+      case double: AnyValue.DoubleValue       => Json.obj("doubleValue" := double.value)
+      case byteArray: AnyValue.ByteArrayValue => Json.obj("bytesValue" := ByteVector(byteArray.value).toBase64)
+      case list: AnyValue.ListValue =>
+        Json.obj("arrayValue" := Json.obj("values" := list.value.map(encodeValue)).dropEmptyValues)
+
+      case map: AnyValue.MapValue =>
+        Json.obj(
+          "kvlistValue" := Json
+            .obj("values" := map.value.map { case (k, v) =>
+              Json.obj("key" := k, "value" := encodeValue(v))
+            })
+            .dropEmptyValues
+        )
+    }
+  }
+}

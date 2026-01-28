15b734cab0d1883073264a0e803ebcc26dc15f11
Maksym Ochenashko <maxochenashko@gmail.com>
2026-01-28 22:40:09 +0200

    semconv: add schema URLs for versions 1.38.0 and 1.39.0, update current schema to 1.39.0

--- semconv/stable/src/main/scala/org/typelevel/otel4s/semconv/SchemaUrls.scala
+++ semconv/stable/src/main/scala/org/typelevel/otel4s/semconv/SchemaUrls.scala
@@ -18,6 +18,8 @@ package org.typelevel.otel4s.semconv
 
 object SchemaUrls {
 
+  val V1_39_0: String = "https://opentelemetry.io/schemas/1.39.0"
+  val V1_38_0: String = "https://opentelemetry.io/schemas/1.38.0"
   val V1_37_0: String = "https://opentelemetry.io/schemas/1.37.0"
   val V1_36_0: String = "https://opentelemetry.io/schemas/1.36.0"
   val V1_35_0: String = "https://opentelemetry.io/schemas/1.35.0"
@@ -31,6 +33,6 @@ object SchemaUrls {
   val V1_27_0: String = "https://opentelemetry.io/schemas/1.27.0"
   val V1_26_0: String = "https://opentelemetry.io/schemas/1.26.0"
 
-  private[otel4s] val Current: String = V1_37_0
+  private[otel4s] val Current: String = V1_39_0
 
 }
